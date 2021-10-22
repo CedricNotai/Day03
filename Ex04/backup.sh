7906c1ce5c3a04a754ad1b7c7014eab49bb61030
Moi <moi>
2021-10-22 12:19:26 +0200

    update day03ex04

--- Ex04/backup.sh
+++ Ex04/backup.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-Current_date=$(date | tr " " "_" | tr ":" "_")
-(sudo crontab -l ; echo "*/2 *  * * * tar -zcvf /Users/admin/Documents/backup/backup_$Current_date.tar.gz /Users/admin/Pictures") | sudo crontab -
+tar -zcvf /Users/admin/Documents/backup/backup_$(date +"%a_%d_%b_%Y_%H_%M_%S_%Z").tar.gz /Users/admin/Pictures
